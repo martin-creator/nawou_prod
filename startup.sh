f2463b36a7cb2ddff00053002c2a13a45f3f9a86
martin-creator <martinlubowa@outlook.com>
2025-07-17 11:34:27 +0300

    Updated

--- startup.sh
+++ startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# --- Paths ---
-APP_ROOT="/home/site/wwwroot"
-NGINX_CONF="$APP_ROOT/default.conf"
-NGINX_TARGET="/etc/nginx/conf.d/default.conf"
 
+echo "Copying custom default.conf over to /etc/nginx/sites-available/default.conf"
+
+NGINX_CONF=/home/site/wwwroot/default.conf
 
 if [ -f "$NGINX_CONF" ]; then
     cp /home/site/wwwroot/default.conf /etc/nginx/sites-available/default
@@ -12,18 +11,32 @@ else
     echo "File does not exist, skipping cp."
 fi
 
-# --- Step 3: PHP-FPM Setup ---
-echo "Setting up PHP-FPM..."
 
-mkdir -p /run/php
-chown www-data:www-data /run/php
-
-pkill -f "php-fpm" || true
-php-fpm -D -y /usr/local/etc/php-fpm.conf
+# # --- Paths ---
+# APP_ROOT="/home/site/wwwroot"
+# NGINX_CONF="$APP_ROOT/default.conf"
+# NGINX_TARGET="/etc/nginx/conf.d/default.conf"
 
 
+# if [ -f "$NGINX_CONF" ]; then
+#     cp /home/site/wwwroot/default.conf /etc/nginx/sites-available/default
+#     service nginx reload
+# else
+#     echo "File does not exist, skipping cp."
+# fi
+
+# # --- Step 3: PHP-FPM Setup ---
+# echo "Setting up PHP-FPM..."
+
+# mkdir -p /run/php
+# chown www-data:www-data /run/php
+
+# pkill -f "php-fpm" || true
+# php-fpm -D -y /usr/local/etc/php-fpm.conf
 
 
-echo "----- Services Started Successfully [$(date)] -----"
-echo "PHP-FPM PID: $(pgrep -f "php-fpm")"
-echo "Nginx PID: $(pgrep "nginx")"
+
+
+# echo "----- Services Started Successfully [$(date)] -----"
+# echo "PHP-FPM PID: $(pgrep -f "php-fpm")"
+# echo "Nginx PID: $(pgrep "nginx")"
