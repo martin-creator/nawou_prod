2a5c21a6d971627c4fe332d7f3ce3ad42a5c6639
martin-creator <martinlubowa@outlook.com>
2025-07-17 20:14:38 +0300

    Used claude to fix errors

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { route as routeFn } from 'ziggy-js';
 import { AppColors } from './lib/utils';
+import axios from 'axios';
 
 declare global {
     const route: typeof routeFn;
@@ -12,12 +13,17 @@ declare global {
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+// Configure axios to use HTTPS
+// axios.defaults.baseURL = window.location.protocol + '//' + window.location.host;
+axios.defaults.baseURL = 'https://' + window.location.host;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
-
+        
         root.render(<App {...props} />);
     },
     progress: {
