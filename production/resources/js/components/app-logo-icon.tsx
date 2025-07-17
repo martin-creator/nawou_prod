f1c270d55e6fa2e10ed686b8620a1648db6b91e4
martin-creator <martinlubowa@outlook.com>
2025-07-17 09:32:05 +0300

    Added wild card for server azure

--- production/resources/js/components/app-logo-icon.tsx
+++
@@ -1,9 +0,0 @@
-import { SVGAttributes } from 'react';
-
-export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return (
-        <svg {...props} xmlns="http://www.w3.org/2000/svg">
-            <image href="/logo.svg" width={"100%"} height={"100%"} />
-        </svg>
-    );
-}
